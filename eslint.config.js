805dfdb1129a2df5b2763e192742ec685c88e791
Jinje Han <coffeetea99iro@gmail.com>
2025-03-15 16:00:01 +0900

    apply prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
   },
 ]);
