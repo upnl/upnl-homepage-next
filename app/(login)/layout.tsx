2a7eeadc575f07efc23652725cfb0938cedaad5e
joongwon <winterdawnlight@snu.ac.kr>
2025-05-03 21:43:45 +0900

    Add main page

--- app/(login)/layout.tsx
+++ app/(login)/layout.tsx
@@ -2,35 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 import "./layout.css";
 import logo from "@/public/images/upnl.png";
-/*
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-    <link rel="stylesheet" href="{{ url_for('static', filename='css/chosen.min.css') }}">
-<script type="text/javascript" src="{{ url_for('static', filename='js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='ckeditor/ckeditor.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/Autolinker.min.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/chosen.jquery.min.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/common.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/typeahead.bundle.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/sisyphus.min.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/jquery.nanoscroller.min.js') }}"></script>
-<script type="text/javascript" src="{{ url_for('static', filename='js/login/layout.js') }}"></script>
-    */
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import CategoryMenu from "./CategoryMenu";
 
 // from date.strftime('%Y/%m/%d %H:%M')
 // TODO: use dayjs
 function formatDate(date: Date) {
   return (
     date.getFullYear() +
-      "/" +
-      String(date.getMonth() + 1).padStart(2, "0") +
-      "/" +
-      String(date.getDate()).padStart(2, "0") +
-      " " +
-      String(date.getHours()).padStart(2, "0") +
-      ":" +
-      String(date.getMinutes()).padStart(2, "0")
+    "/" +
+    String(date.getMonth() + 1).padStart(2, "0") +
+    "/" +
+    String(date.getDate()).padStart(2, "0") +
+    " " +
+    String(date.getHours()).padStart(2, "0") +
+    ":" +
+    String(date.getMinutes()).padStart(2, "0")
   );
 }
 
@@ -40,8 +28,7 @@ export default async function LoginLayout({
   children: React.ReactNode;
 }>) {
   const session = await getServerSession();
-  if (!session)
-    redirect("/");
+  if (!session) redirect("/");
 
   /* TODO: data fetching */
   const user = {
@@ -54,120 +41,11 @@ export default async function LoginLayout({
     { bbs_url: "1", bbs_name: "공지사항" },
     { bbs_url: "2", bbs_name: "자유게시판" },
   ];
-  const allTagList = [
-    { no: 1, content: "tag1", count: 5 },
-    { no: 2, content: "tag2", count: 10 },
-  ];
-  const panoramaList = [
-    {
-      action: { value: "new-article" },
-      article: {
-        no: 1, title: "Test Article", bbs_name: "자유게시판", bbs_url: "2",
-        user: { no: 1, name: "Admin" },
-      },
-      actor: { no: 1, name: "Admin", email: "admin@example.com" },
-      date: new Date(),
-    },
-    {
-      action: { value: "new-comment" },
-      article: {
-        no: 1, title: "Test Article", bbs_name: "자유게시판", bbs_url: "2",
-        user: { no: 1, name: "Admin" },
-      },
-      actor: { no: 1, name: "Admin", email: "admin@example.com" },
-      date: new Date(),
-    },
-  ];
   const lastCommit = "abcdef";
   const lastCommitDate = new Date();
 
   // TODO: utilties
-  const tagLevel = (_count: number) => "";
-  const makeGravatarUrl = (_email: string, _size: number) => "https://example.com";
   const prettyDate = (date: Date) => formatDate(date);
-
-  // TODO: jinja blocks
-  const blockAddSidebar = null;
-  const blockSidebar = (
-    <div id="sidebar">
-      {blockAddSidebar}
-      <div id="tagcloud">
-        <div className="title_horizontal">
-          <h3>태그</h3>
-        </div>
-        <div className="content_horizontal">
-          <div id="tag_contents" className="nano">
-            <div className="nano-content">
-              {allTagList.map((tag) => (
-                <span className="bar_separated" key={tag.no}>
-                  <a className={tagLevel(tag.count)} href={`/tag/${tag.no}`}>
-                    {tag.content}
-                  </a>
-                </span>
-              ))}
-            </div>
-          </div>
-          <div id="tag_search" className="search">
-            <input id="tag_search_content" type="text"/>
-            <button id="tag_search_button">검색</button>
-          </div>
-          <div style={{clear: "both"}}></div>
-        </div>
-      </div>
-    </div>
-  );
-  const blockContents = children;
-  const blockPanorama = (
-    <>
-      <button id="panorama-wake-button">파노라마</button>
-      <div id="panorama">
-        <div className="title_horizontal">
-          <h3>파노라마</h3>
-        </div>
-        <div id="panorama_content" className="content_horizontal">
-          <div id="panorama_nano" className="nano">
-            <div id="panorama_nano_content" className="nano-content">
-              {panoramaList.map((panorama, index) => (
-                panorama.article === null ?
-                  <div className="panorama_row article" key={index}>
-                    <span>삭제된 게시글입니다.</span>
-                  </div>
-                  :
-                  <div className="panorama_row article" key={index}>
-                    <img src={makeGravatarUrl(panorama.actor.email, 32)} height="32px" />
-                    <div className="panorama-row-content">
-                      <Link href={`/${panorama.article.no}`}>
-                        {panorama.action.value === "new-article" ?
-                          `${panorama.actor.name} 님의 글 [${panorama.article.title}]이(가) 등록되었습니다.` :
-                          panorama.action.value === "new-comment" ?
-                          `${panorama.actor.name} 님이 글 [${panorama.article.title}]에 댓글을 다셨습니다.` :
-                          null
-                        }
-                      </Link>
-                      /
-                      {panorama.article.bbs_name === "지름게시판" ?
-                        (<>
-                          <Link href="/purchases" className="panorama_board">
-                            {panorama.article.bbs_name}
-                          </Link>
-                          <br />
-                        </>) :
-                          (<Link href={`/board/${panorama.article.bbs_url}`} className="panorama_board">
-                            {panorama.article.bbs_name}
-                          </Link>)
-                      }
-                      <span title={formatDate(panorama.date)} className="datatime">
-                        ({prettyDate(panorama.date)})
-                      </span>
-                    </div>
-                  </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
 
   return (
     <div id="body">
@@ -176,12 +54,14 @@ export default async function LoginLayout({
       <div id="header" className="default">
         <div id="top_menu" className="dark">
           <div id="menu_menu">
-            {user.id == 'admin' && (<>
-              <span>
-                <Link href="/admin">관리자 페이지</Link>
-              </span>
-              <span className="bar">|</span>
-            </>)}
+            {user.id == "admin" && (
+              <>
+                <span>
+                  <Link href="/admin">관리자 페이지</Link>
+                </span>
+                <span className="bar">|</span>
+              </>
+            )}
             <span>
               <Link href="/logout">로그아웃</Link>
             </span>
@@ -192,7 +72,7 @@ export default async function LoginLayout({
           </div>
           <div id="menu_user_info">
             <span id="menu_user_gravatar">
-              <img src={gravatarUrl}/>
+              <img src={gravatarUrl} />
             </span>
             <span id="menu_username">
               <Link href={`/user_info/${user.no}`}>{user.name}</Link>
@@ -209,73 +89,45 @@ export default async function LoginLayout({
             </h2>
           </div>
         </div>
-        <ul id="category">
-          <li id="category_various" className="category_content">
-            <Link href="/board/2">게시판</Link>
-          </li>
-          <li id="category_study" className="category_content">
-            <Link href="/study">스터디</Link>
-          </li>
-          <li id="category_project" className="category_content">
-            <Link href="/projects">프로젝트</Link>
-          </li>
-          <li id="category_workshop" className="category_content">
-            <Link href="/workshops/0">워크샵</Link>
-          </li>
-          <li id="category_calendar" className="category_content">
-            <Link href="/calendar">일정</Link>
-          </li>
-        </ul>
-        <div id="content_line">
-          <div id="nav_button_left">◁</div>
-          <div id="line_various">
-            {boardList.map((board) => (
-              <span className="bar_separated" key={board.bbs_url}>
-                <Link href={`/board/${board.bbs_url}`}>{board.bbs_name}</Link>
-              </span>
-            ))}
-            <span className="bar_separated"><Link href="/purchases">지름게시판</Link></span>
-            <span className="bar_separated"><Link href="/promotes">정회원 승격 신청</Link></span>
-          </div>
-          <div id="nav_button_right">▷</div>
-        </div>
+        <CategoryMenu boardList={boardList} />
       </div>
       {/*헤더끝*/}
-      <div id="middle" className="container backboard">
-        {blockSidebar}
-        <div id="main_contents">
-          {blockContents}
-        </div>
-        {blockPanorama}
-      </div>
+      {children}
       {/*푸터시작*/}
       <div id="bottom_menu" className="default dark">
-        <span><a href="https://github.com/upnl/upnl-homepage-next/issues">오류 신고 / 기능 제안</a></span>
-        <span className="bar">|</span>
-        <span>
+        <span className="bar_separated">
+          <a href="https://github.com/upnl/upnl-homepage-next/issues">
+            오류 신고 / 기능 제안
+          </a>
+        </span>
+        <span className="bar_separated">
           <Link href="/users">회원 정보</Link>
         </span>
-        <span className="bar">|</span>
-        <span>
+        <span className="bar_separated">
           <Link href="http://wiki.sodrak.upnl.org">위키</Link>
         </span>
-        <span className="bar">|</span>
-        <span>
+        <span className="bar_separated">
           <Link href="/panorama">파노라마</Link>
         </span>
-        <span className="bar">|</span>
-        <span>
+        <span className="bar_separated">
           {/* TODO: what is this? */}
           <Link href="/andromeda">안드로메다</Link>
         </span>
-        <span className="bar">|</span>
-        <span><Link href="/public/bylaw">UPnL 회칙</Link></span>
-        <span className="bar">|</span>
-        <Link href="/recent.atom">Atom</Link>
+        <span className="bar_separated">
+          <Link href="/public/bylaw">UPnL 회칙</Link>
+        </span>
+        <span className="bar_separated">
+          <Link href="/recent.atom">Atom</Link>
+        </span>
         <div className="about-commit">
           {lastCommit.length > 0 && (
             <span>
-              마지막 커밋: <Link href={`https://github.com/upnl/upnl-homepage-next/commit/${lastCommit}`}>{lastCommit.slice(0, 5)}</Link>
+              마지막 커밋:{" "}
+              <Link
+                href={`https://github.com/upnl/upnl-homepage-next/commit/${lastCommit}`}
+              >
+                {lastCommit.slice(0, 5)}
+              </Link>
               (최근 수정: {prettyDate(lastCommitDate)})
             </span>
           )}
--- app/(login)/main/page.tsx
+++ app/(login)/main/page.tsx
@@ -1,3 +1,316 @@
+import {
+  Container,
+  MainAlone,
+  Panorama,
+  TagCloud,
+} from "@/components/internalTemplates";
+import Link from "next/link";
+import "@/public/css/border_text.css";
+import "./page.css";
+
+/*
+ * TODO: use dayjs
+ * date.strftime('%m/%d')
+ */
+function formatDate(date: Date) {
+  return (
+    String(date.getMonth() + 1).padStart(2, "0") +
+    "/" +
+    String(date.getDate()).padStart(2, "0")
+  );
+}
+
+/*
+ * TODO: use dayjs
+ * date.strftime('%Y.%m.%d')
+ */
+function formatWorkshopDate(date: Date) {
+  return (
+    date.getFullYear() +
+    "." +
+    String(date.getMonth() + 1).padStart(2, "0") +
+    "." +
+    String(date.getDate()).padStart(2, "0")
+  );
+}
+
+function isNew(date: Date) {
+  // TODO
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+  const diffHours = Math.floor(diff / (1000 * 60 * 60));
+  return diffHours < 24;
+}
+
 export default async function LoginIndex() {
-  return null;
+  const articles_notice = [
+    {
+      no: 1,
+      date: new Date(),
+      title: "공지사항 제목",
+      comment: [],
+    },
+    {
+      no: 2,
+      date: new Date(),
+      title: "공지사항 제목2",
+      comment: [],
+    },
+  ];
+  const articles_free = [
+    {
+      no: 3,
+      date: new Date(),
+      title: "자유게시판 제목",
+      comment: [],
+    },
+    {
+      no: 4,
+      date: new Date(),
+      title: "자유게시판 제목2",
+      comment: [],
+    },
+  ];
+  const purchase_list = [
+    {
+      no: 5,
+      article: {
+        no: 5,
+        date: new Date(),
+        title: "지름게시판 제목",
+      },
+      product_name: "제품명",
+      assenter: [],
+    },
+    {
+      no: 6,
+      article: {
+        no: 6,
+        date: new Date(),
+        title: "지름게시판 제목2",
+      },
+      product_name: "제품명2",
+      assenter: [],
+    },
+  ];
+  const promote_list = [
+    {
+      requester_name: "요청자 이름",
+      assenter: [],
+      bbs_url: 7,
+    },
+    {
+      requester_name: "요청자 이름2",
+      assenter: [],
+      bbs_url: 8,
+    },
+  ];
+  const articles_project = [
+    {
+      no: 9,
+      bbs_name: "프로젝트 이름",
+      date: new Date(),
+      comment: [],
+      title: "제목",
+    },
+  ];
+  const workshop_portion = [
+    {
+      nth: 1,
+      start_date: new Date(),
+      sessions: [
+        {
+          no: 1,
+          title: "세션 제목",
+        },
+        {
+          no: 2,
+          title: "세션 제목2",
+        },
+      ],
+    },
+  ];
+  const required = 5;
+  return (
+    <Container>
+      <MainAlone>
+        <div id="main_new_articles" className="container nonedeco">
+          <div className="title_horizontal">
+            <h3>최신 글</h3>
+          </div>
+          <div id="main_notice" className="content_horizontal">
+            <h4 id="main_notice_title">
+              <Link href="/board/1">공지사항</Link>
+            </h4>
+            <div id="main_notice_contents" className="nano">
+              <div id="main_notice_nano_content" className="nano-content">
+                {articles_notice.map((article) => (
+                  <div className="article_row" key={article.no}>
+                    <span className="date">{formatDate(article.date)}</span>
+                    <div className="article_row_content">
+                      <span className="article_title">
+                        <Link href={`/${article.no}`}>{article.title}</Link>
+                      </span>
+                      {article.comment.length !== 0 && (
+                        <span className="comment">
+                          [{article.comment.length}]
+                        </span>
+                      )}
+                      <span className="is_new">
+                        {isNew(article.date) && "★"}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div id="main_freeboard" className="content_horizontal">
+            <h4 id="main_freeboard_title">
+              <Link href="/board/2">자유게시판</Link>
+            </h4>
+            <div id="main_freeboard_contents" className="nano">
+              <div id="main_freeboard_nano_content" className="nano-content">
+                {articles_free.map((article) => (
+                  <div className="article_row" key={article.no}>
+                    <span className="date">{formatDate(article.date)}</span>
+                    <div className="article_row_content">
+                      <span className="article_title">
+                        <Link href={`/${article.no}`}>{article.title}</Link>
+                      </span>
+                      {article.comment.length !== 0 && (
+                        <span className="comment">
+                          [{article.comment.length}]
+                        </span>
+                      )}
+                      <span className="is_new">
+                        {isNew(article.date) && "★"}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div id="main_purchase" className="content_horizontal">
+            <h4 className="main_purchase_title">
+              <Link href="/purchases" className="main_purchase_title">
+                지름게시판
+              </Link>
+            </h4>
+            <div id="main_purchase_contents" className="nano">
+              <div id="main_purchase_nano_content" className="nano-content">
+                {purchase_list.map((purchase) => (
+                  <div className="purchase_row article" key={purchase.no}>
+                    <span className="date">
+                      {formatDate(purchase.article.date)}
+                    </span>
+                    <span className="purchase_row_title">
+                      <Link href={`/${purchase.article.no}`}>
+                        {purchase.article.title}
+                      </Link>
+                    </span>
+                    <span className="num_agree">
+                      {" "}
+                      - 동의 수 : {purchase.assenter.length} / {required}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div id="main_promote" className="content_horizontal">
+            <h4 id="main_promote_title">
+              <Link href="/promotes">승격게시판</Link>
+            </h4>
+            <div id="main_promote_contents" className="nano">
+              <div id="main_promote_nano_content" className="nano-content">
+                {promote_list.map((promote) => (
+                  <div className="promote_row article" key={promote.bbs_url}>
+                    <span className="promote_row_title">
+                      <Link href={`/board/${promote.bbs_url}`}>
+                        {promote.requester_name}
+                      </Link>
+                    </span>
+                    <span className="num_agree">
+                      {" "}
+                      - 동의 수 : {promote.assenter.length} / {required}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="main_users_activities" className="nonedeco">
+          <div className="title_horizontal">
+            <h3 id="main_user_activities_title">참여 중인 프로젝트 & 스터디</h3>
+          </div>
+          <div className="content_horizontal">
+            <div id="main_user_activities_contents" className="nano">
+              <div
+                id="main_user_activities_nano_content"
+                className="nano-content"
+              >
+                {articles_project.map((article) => (
+                  <div
+                    className="project_article container article"
+                    key={article.no}
+                  >
+                    <h4>{article.bbs_name}</h4>
+                    <span className="date">{formatDate(article.date)}</span>
+                    <div className="project_row_content">
+                      <span className="project_title">
+                        <Link href={`/${article.no}`}>{article.title}</Link>
+                      </span>
+                      {article.comment.length !== 0 && (
+                        <span className="comment">
+                          [{article.comment.length}]
+                        </span>
+                      )}
+                      <span className="is_new">
+                        {isNew(article.date) && "★"}
+                      </span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <TagCloud />
+        <div id="workshop">
+          <div className="title_vertical">
+            <h3>워크샵</h3>
+          </div>
+          {workshop_portion.map((workshop) => (
+            <div className="content_vertical" key={workshop.nth}>
+              <div className="workshop_title">
+                <h4>
+                  <Link href={`/workshops/${workshop.nth}`}>
+                    {workshop.nth}차 워크샵
+                  </Link>
+                </h4>
+              </div>
+              <div className="workshop_datatime">
+                {formatWorkshopDate(workshop.start_date)}
+              </div>
+              <div className="workshop_about nano">
+                <div className="nano-content">
+                  {workshop.sessions
+                    .filter((session) => session.title !== "")
+                    .map((session) => (
+                      <div className="session_title" key={session.no}>
+                        {session.title}
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </MainAlone>
+      <Panorama />
+    </Container>
+  );
 }
